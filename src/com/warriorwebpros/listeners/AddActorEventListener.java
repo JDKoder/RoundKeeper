205361cd8313ff5f87b8fbf16dc4cfa1e1387a03
JDKoder <jake.krieger@gmail.com>
2013-04-14 22:31:10 -0400

    Removing Dead Code

--- src/com/warriorwebpros/listeners/AddActorEventListener.java
+++
@@ -1,9 +0,0 @@
-package com.warriorwebpros.listeners;
-
-import java.util.EventObject;
-
-import com.warriorwebpros.model.Actor;
-
-public interface AddActorEventListener{
-	public void handleAddNewActorEvent(EventObject event, Actor actor);
-}
